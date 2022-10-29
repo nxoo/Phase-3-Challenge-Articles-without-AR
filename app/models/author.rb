7ce5322f932c535f693aac231b378a3da091ef7a
nxoo <nxoo.dev@gmail.com>
2022-10-29 10:15:25 +0300

    article & author:

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,26 @@
 class Author
-  attr_accessor :name
+  attr_reader :name
 
+  @@all = []
 
   def initialize(name)
     @name = name
-  
+    @@all << self
   end
 
+  def add_article (magazine, title)
+    Article.new(self, magazine, title)
+  end
 
+  def articles
+    Article.all.select { |article| article.author == self }
+  end
+
+  def magazines
+    articles.map(&:magazine).uniq
+  end
+
+  def topic_areas
+    articles.collect { |art| art.magazine.category }.uniq
+  end
 end
