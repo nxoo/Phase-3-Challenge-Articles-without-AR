c0e710737cc45f62988c00186a8a024343dd543e
nxoo <nxoo.dev@gmail.com>
2022-10-29 16:37:47 +0300

    magazine

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,45 @@
 class Magazine
-  attr_accessor :name, :category
-
+  attr_reader :name, :category
+  :@all = []
 
   def initialize(name, category)
     @name = name
     @category = category
-
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def add_article(title, author)
+    Article.new(author, self, title)
+  end
+
+  def articles
+    Article.all.select { |article| article.magazine == self }
+  end
+
+  def article_titles
+    articles.map { |article| article.title }
+  end
+
+  def contributors
+    articles.map { |art| art.author }.uniq
+  end
+
+  def contributing_authors
+    authors = articles.map do |article|
+      if article.author.articles.length > 2
+        article.author
+      end
+    end
+    authors.uniq
+  end
+
+  def self.find_by_name(name)
+    magazine = self.all.find { |magazine| magazine.name == name }
+    magazine.name
+  end
 
 end
